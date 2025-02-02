2102f24e2d1a5e53944435d1203ddfa2702e4aa1
J Leadbetter <j@jleadbetter.com>
2025-02-02 13:16:43 +0100

    f

--- common/models/users.py
+++ common/models/users.py
@@ -3,53 +3,50 @@ Copyright (C) J Leadbetter <j@jleadbetter.com>
 Affero GPL v3
 """
 
-import time
 import uuid
-from typing import Any, Optional
+from typing import Optional
 
 from pydantic import BaseModel
 
 from .base import HashableMixin
 
 
-class UserDB(HashableMixin, BaseModel):
+class UserBase(HashableMixin):
+    """
+    Shared base class for both Pydantic and Django models for the database.
+
+    Must implement the following fields:
+
+    * id
+    * user (with user.id)
+    * language_code
+    """
+    pass
+
+
+class UserDB(UserBase, BaseModel):
     """
     Representation of a user in the database.
     """
 
-    id: Optional[str] = None
+    id: Optional[uuid.UUID] = None
     username: str
     password: Optional[str] = None
-    email: Optional[str] = None
-    display_name: str
+    display_name: Optional[str] = None
+    is_admin: Optional[bool] = False
 
     @property
     def unique_fields(self):
         return ['username']
 
 
-class UserUI(HashableMixin, BaseModel):
+class UserUI(UserBase, BaseModel):
     """
     Representation of a logged-in user in the UI.
     NOTE: use camel-cased attributes for easier handling with javascript
     """
 
-    id: str  # UUID
+    id: uuid.UUID
     username: str
     displayName: Optional[str] = None
-
-    @classmethod
-    def from_db(cls, user: UserDB) -> 'UserUI':
-        """
-        Convert a database user into a UI-friendly user object.
-
-        :user: User object from the database
-        :return: UserDisplay for the UI.
-        """
-
-        user_ui = cls(
-            id=user.id,
-            username=user.username,
-            displayName=user.display_name or user.username,
-        )
-        return user_ui
+    is_admin: Optional[bool] = False
